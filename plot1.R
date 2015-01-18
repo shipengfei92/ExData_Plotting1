23cbf38d85d5ece909841c34ee69a316a9641af2
Pengfei Shi <shipengfei92@gmail.com>
2015-01-19 00:01:48 +0800

    plot1

---
+++ plot1.R
@@ -0,0 +1,10 @@
+setwd("./coursera/exploratory-data-analysis/")
+data <- read.csv("household_power_consumption.txt",header = TRUE,sep = ";",na.strings = "?")
+data$Date <- as.Date(data$Date,format="%d/%m/%Y")
+data <- subset(data,subset = (Date>="2007-02-01" & Date <= "2007-02-02"))
+Time <- paste(as.Date(data$Date),data$Time)
+data$Time <- as.POSIXct(Time)
+hist(data$Global_active_power,main = "Global Active Power",xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
+
+dev.copy(png,file="plot1.png",height=480,width=480)
+dev.off()
